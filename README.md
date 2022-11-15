415efa4a70afb6b0226e12b2e7ea66e0a557eb51
Roni Lazimi <rlazimi@ronis-mbp.lan>
2022-11-14 20:17:34 -0500

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pokemon Type Advantage Cycles
+
+[![Pokemon Type Advantage Cycles](https://img.youtube.com/vi/zs1iYJlcqPo/maxresdefault.jpg)](https://www.youtube.com/watch?v=zs1iYJlcqPo)
---
+++ poketypes.py
@@ -0,0 +1,94 @@
+NORMAL = 0
+FIRE = 1
+WATER = 2
+ELECTRIC = 3
+GRASS = 4
+ICE = 5
+FIGHTING = 6
+POISON = 7
+GROUND = 8
+FLYING = 9
+PSYCHIC = 10
+BUG = 11
+ROCK = 12
+GHOST = 13
+DRAGON = 14
+DARK = 15
+STEEL = 16
+FAIRY = 17
+
+advantages = [
+    [],                                     # NORMAL
+    [GRASS, ICE, BUG, STEEL],               # FIRE
+    [FIRE, GROUND, ROCK],                   # WATER
+    [WATER, FLYING],                        # ELECTRIC
+    [WATER, GROUND, ROCK],                  # GRASS
+    [GRASS, GROUND, FLYING, DRAGON],        # ICE
+    [NORMAL, ICE, ROCK, DARK, STEEL],       # FIGHTING
+    [GRASS, FAIRY],                         # POISON
+    [FIRE, ELECTRIC, POISON, ROCK, STEEL],  # GROUND
+    [GRASS, FIGHTING, BUG],                 # FLYING
+    [FIGHTING, POISON],                     # PSYCHIC
+    [GRASS, PSYCHIC, DARK],                 # BUG
+    [FIRE, ICE, FLYING, BUG],               # ROCK
+    [PSYCHIC, GHOST],                       # GHOST
+    [DRAGON],                               # DRAGON
+    [PSYCHIC, GHOST],                       # DARK
+    [ICE, ROCK, FAIRY],                     # STEEL
+    [FIGHTING, DRAGON, DARK],               #FAIRY
+]
+
+
+id_to_type = [
+    'normal',
+    'fire',
+    'water',
+    'electric',
+    'grass',
+    'ice',
+    'fighting',
+    'poison',
+    'ground',
+    'flying',
+    'psychic',
+    'bug',
+    'rock',
+    'ghost',
+    'dragon',
+    'dark',
+    'steel',
+    'fairy'
+]
+
+type_chart = [
+    #N    #FI  #W   #GRA #E   #I   #FI  #PO  #GRO #FL  #PS  #B   #R   #GH  #DR  #DA  #S   #FA
+    [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.0, 1.0, 1.0, 0.5, 1.0], # NORMAL
+    [1.0, 0.5, 0.5, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 0.5, 1.0, 2.0, 1.0], # FIRE
+    [1.0, 2.0, 0.5, 1.0, 0.5, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0, 1.0, 1.0], # WATER
+    [1.0, 1.0, 2.0, 0.5, 0.5, 1.0, 1.0, 1.0, 0.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0], # GRASS
+    [1.0, 0.5, 2.0, 1.0, 0.5, 1.0, 1.0, 0.5, 2.0, 0.5, 1.0, 0.5, 2.0, 1.0, 0.5, 1.0, 0.5, 1.0], # ELECTRIC
+    [1.0, 0.5, 0.5, 1.0, 2.0, 0.5, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0], # ICE
+    [2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0, 0.5, 0.5, 0.5, 2.0, 0.0, 1.0, 2.0, 2.0, 0.5], # FIGHTING
+    [1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 0.0, 2.0], # POISON
+    [1.0, 2.0, 1.0, 2.0, 0.5, 1.0, 1.0, 2.0, 1.0, 0.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 2.0, 1.0], # GROUND
+    [1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 0.5, 1.0], # FLYING
+    [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 0.0, 0.5, 1.0], # PSYCHIC
+    [1.0, 0.5, 1.0, 1.0, 2.0, 1.0, 0.5, 0.5, 1.0, 0.5, 2.0, 1.0, 1.0, 0.5, 1.0, 2.0, 0.5, 0.5], # BUG
+    [1.0, 2.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 0.5, 2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0], # ROCK
+    [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0, 1.0], # GHOST
+    [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 0.5, 0.0], # DRAGON
+    [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 2.0, 1.0, 0.5, 1.0, 0.5], # DARK
+    [1.0, 0.5, 0.5, 0.5, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 0.5, 2.0], # STEEL
+    [1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 0.5, 1.0]  # FAIRY
+]
+
+
+def reversable(cycle):
+    if len(cycle) < 2:
+        return False
+    for i in range(len(cycle)-1,-1,-1):
+        attacker = cycle[i]
+        victim = cycle[i-1]
+        if type_chart[attacker][victim] > 0.5:
+            return False
+    return True
